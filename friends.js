f562cfe5d033353db7a9cd56f4074b52c9cb2a65
Kass Daniello <zertroz@gmail.com>
2022-11-10 19:21:19 -0500

    Add second for statement

--- friends.js
+++ friends.js
@@ -1,7 +1,13 @@
-var someNums = [8, 17, 35, 42, 58, 73, 80, 99]
+var someNums = [8, 17, 35, 42, 58, 73, 80, 99];
 
 for (var i = 0; i < 100; i++) {
     if (someNums[i] > 30) {
-        console.log(someNums[i])
+        console.log(someNums[i]);
     }
-}
+};
+console.log("=========")
+for (var i = 0; i < 100; i++) {
+    if (someNums[i] > 60) {
+        console.log(someNums[i]);
+    }
+};
